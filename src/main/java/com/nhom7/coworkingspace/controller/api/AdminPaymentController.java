adf2a8a965bd661048c86fdf0dc666cf6cd7edc7
Hieutran0106 <hieutrant0106@gmail.com>
2026-08-25 02:45:28 +0700

    docs: improve swagger controller tags

--- src/main/java/com/nhom7/coworkingspace/controller/api/AdminPaymentController.java
+++ src/main/java/com/nhom7/coworkingspace/controller/api/AdminPaymentController.java
@@ -9,11 +9,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@Tag(name = "Admin Payment", description = "Admin payment management")
 @RequiredArgsConstructor
 public class AdminPaymentController {
 
